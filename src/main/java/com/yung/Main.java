8f3980952e817ea19ccd6b92f49b11a0ef932d31
jyung17 <iotaxixi@gmail.com>
2022-11-27 15:00:21 +0900

    2675: 문자열 반복

--- src/main/java/com/yung/Main.java
+++ src/main/java/com/yung/Main.java
@@ -1,8 +1,32 @@
 package com.yung;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main {
   
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    int T = Integer.parseInt(br.readLine());
+    
+    for(int i = 0; i < T; i++) {
+      
+      String[] str = br.readLine().split(" ");	// 공백 분리
+      
+      int R = Integer.parseInt(str[0]);	// String -> int
+      String S = str[1];
+      
+      for(int j = 0; j < S.length(); j++) {
+        for(int k = 0; k < R; k++) {
+          System.out.print(S.charAt(j));
+        }
+      }
+      System.out.println();
+    }
   }
+  
+  
 }
