1533a6c540c4b3bcdb4ae8cb4dd4a39cd70cf1f8
jyung17 <iotaxixi@gmail.com>
2022-12-02 19:41:46 +0900

    2231: 분해합 https://www.acmicpc.net/problem/2231

--- src/main/java/com/yung/Main_2231.java
+++ src/main/java/com/yung/Main_2231.java
@@ -8,7 +8,7 @@ package com.yung;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main_2231 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt(); // n: 256
