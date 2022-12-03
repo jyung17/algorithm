d69512569dcbc87635923892199df7ab353a9f0d
jyung17 <iotaxixi@gmail.com>
2022-12-03 18:51:56 +0900

    2798: 블랙잭 https://www.acmicpc.net/problem/2798

--- src/main/java/com/yung/Main_2798.java
+++ src/main/java/com/yung/Main_2798.java
@@ -7,7 +7,7 @@ package com.yung;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class Main_2798 {
   public static void main(String[] args) throws IOException {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
@@ -15,7 +15,7 @@ public class Main {
     int[] value = new int[n];
     int sum = 0;
     int result = 0;
-  
+    
     for (int i = 0; i < n; i++) {
       value[i] = scanner.nextInt();
     }
