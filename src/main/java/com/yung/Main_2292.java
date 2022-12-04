32baef7edf86bae23af98c9acdd06c876864a1b6
jyung17 <iotaxixi@gmail.com>
2022-12-04 17:38:38 +0900

    2292: 벌집 https://www.acmicpc.net/problem/2292

--- src/main/java/com/yung/Main_2292.java
+++ src/main/java/com/yung/Main_2292.java
@@ -7,7 +7,7 @@ package com.yung;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class Main_2292 {
   public static void main(String[] args) throws IOException {
    Scanner scanner = new Scanner(System.in);
    
