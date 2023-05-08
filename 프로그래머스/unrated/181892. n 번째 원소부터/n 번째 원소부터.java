49ebfc6f70fa57eb78d19858a5c1a3542b759be2
YOUNG <68783226+jyung17@users.noreply.github.com>
2023-05-08 22:01:31 +0900

    [unrated] Title: n 번째 원소부터, Time: 2.33 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = n-1; i < num_list.length; i++) {
+          answer.add(num_list[i]);
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
